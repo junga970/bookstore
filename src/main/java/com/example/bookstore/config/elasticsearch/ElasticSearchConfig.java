e15d3de596ab3ffe218daa283da38ffaaf3e9f25
junga970 <junga2940@gmail.com>
2023-01-23 03:17:22 +0900

    Chore: docker-compose.yml 에 bookstore app 추가

--- src/main/java/com/example/bookstore/config/elasticsearch/ElasticSearchConfig.java
+++ src/main/java/com/example/bookstore/config/elasticsearch/ElasticSearchConfig.java
@@ -1,6 +1,7 @@
 package com.example.bookstore.config.elasticsearch;
 
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
@@ -10,11 +11,17 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
 
+	@Value("${elasticsearch.host}")
+	private String host;
+
+	@Value("${elasticsearch.port}")
+	private int port;
+
 	@Override
 	public RestHighLevelClient elasticsearchClient() {
 
 		ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-			.connectedTo("localhost:9200")
+			.connectedTo(host + ":" + port)
 			.build();
 
 		return RestClients.create(clientConfiguration).rest();
